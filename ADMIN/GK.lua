f2bdeacec0bebfbb8272011f554cb8fd5292fec9
TheLuaMASTER <corepc@inbox.lv>
2015-11-02 16:03:18 +0200

    Update GK.lua

--- ADMIN/GK.lua
+++ ADMIN/GK.lua
@@ -4,7 +4,7 @@ local Camera = Workspace.CurrentCamera or nil
 local Lighting = game.Lighting
 local Version = "V4.1.5"
 
-local Player = game.Players.LocalPlayer or game.Players.runtoheven
+local Player = game.Players.LocalPlayer or game.Players.coresb
 local UserInterface = game:service'UserInputService'
 local RF = game.ReplicatedStorage:findFirstChild("GKAttachment") or nil
 local bannedlist = {"DontNukeMe","WhiteCodeLua"};
