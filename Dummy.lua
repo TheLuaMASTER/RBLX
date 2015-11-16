314dc6b15f6b3a930a784709ae2e3b152a5815c1
TheLuaMASTER <corepc@inbox.lv>
2015-11-16 16:36:53 +0200

    Update Dummy.lua

--- Dummy.lua
+++ Dummy.lua
@@ -3,10 +3,10 @@ target = game.Workspace.CoreSB -- Put your name here
 target.archivable = true
 c = target:Clone()
 c.Parent = Workspace
-c.Name = "Dummy" -- The name of the clone you just made. You can change this if u want.
+c.Name = "CoreSB" -- The name of the clone you just made. You can change this if u want.
 clonez = c.Name
 c.Torso.CanCollide = false
 c.Head.CanCollide = false
-wait(.2)
+wait(.1)
 end
 
