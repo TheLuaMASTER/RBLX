c2fad095cae502d7cc01b2ec01e91c0e8140bb24
TheLuaMASTER <corepc@inbox.lv>
2015-12-05 14:16:55 +0200

    Update and rename MLG_Tabs.lua to CoreTabs.lua

---
+++ ADMIN/CoreTabs.lua
@@ -0,0 +1,6 @@
+print("Core Tabs Loading")
+wait(.3)
+local Player = game.Players.LocalPlayer or game.Players.CoreSB
+local Mouse = Player:GetMouse() or nil
+local Version = "V1.0.0 Beta"
+local bannedlist = {"5ZR"}
